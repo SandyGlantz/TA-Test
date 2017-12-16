117910e21c250c62a4e04884d6f4d04159de38ec
Sandy Glantz <sandyglantz@gmail.com>
2017-12-16 02:48:52 -0800

    Added in more efficient version under division

--- Challenge2_Calculator_C#_DevU/CS-ASP_010_Final/MySimpleCalculator/MySimpleCalculator/Default.aspx.cs
+++ Challenge2_Calculator_C#_DevU/CS-ASP_010_Final/MySimpleCalculator/MySimpleCalculator/Default.aspx.cs
@@ -49,13 +49,21 @@ namespace MySimpleCalculator
 
         protected void diviButton_Click(object sender, EventArgs e)
         {
-            string nText1 = firstNumTextBox.Text;
-            double num1 = double.Parse(nText1);
-            string nText2 = secondNumTextBox.Text;
-            double num2 = double.Parse(nText2);
+            /*    string nText1 = firstNumTextBox.Text;
+                double num1 = double.Parse(nText1);
+                string nText2 = secondNumTextBox.Text;
+                double num2 = double.Parse(nText2);
 
+                double r = num1 / num2;
+                resultLabel.Text = r.ToString();  */
+
+      // Instructor version:
+      // Consolidated actions = fewer lines code, fewer variables
+            double num1 = double.Parse(firstNumTextBox.Text);
+            double num2 = double.Parse(secondNumTextBox.Text);
             double r = num1 / num2;
             resultLabel.Text = r.ToString();
-        }
+        }     
+         
     }
 }

63923caaad905d867be0807589d2d74ab96d6532
Sandy Glantz <sandyglantz@gmail.com>
2017-12-22 01:33:22 -0800

    Commented on methods in exercise for furure reference

--- Challenge5_Dates_C#_DevU/CS-ASP_014_DatesTimes/CS-ASP_014_DatesTimes/Default.aspx.cs
+++ Challenge5_Dates_C#_DevU/CS-ASP_014_DatesTimes/CS-ASP_014_DatesTimes/Default.aspx.cs
@@ -17,25 +17,71 @@ namespace CS_ASP_014
         protected void okButton_Click(object sender, EventArgs e)
         {
 
+            
+             // ===== Create the variable with the datatype: ===== 
+             DateTime myValue = DateTime.Now;
 
-            //DateTime myValue = DateTime.Now;
-            //resultLabel.Text = myValue.ToString();
-            //resultLabel.Text = myValue.ToLongDateString();
-            //resultLabel.Text = myValue.ToLongTimeString();
-            //resultLabel.Text = myValue.ToShortDateString();
-            //resultLabel.Text = myValue.ToShortTimeString();
 
-            //resultLabel.Text = myValue.AddDays(2).ToString();
-            //resultLabel.Text = myValue.AddMonths(-2).ToString();
+             //  ===== Convert DateTime datatype ===== 
+             resultLabel.Text = myValue.ToString();
+             // returns 12/22/17 12:38:57 AM
 
-            //resultLabel.Text = myValue.Month.ToString();
-            //resultLabel.Text = myValue.IsDaylightSavingTime().ToString();
-            //resultLabel.Text = myValue.DayOfWeek.ToString();
-            //resultLabel.Text = myValue.DayOfYear.ToString();
 
-            //DateTime myValue = DateTime.Parse("12/7/1969");
-            DateTime myValue = new DateTime(1969, 12, 7, 6, 30, 0);
+             // =====  These are a few of the methods ===== 
+
+             resultLabel.Text = myValue.ToLongDateString();
+             // returns December 22, 2017
+             // NOTE: Instructor's ToLongDateString includes day of week at beginning
+
+             resultLabel.Text = myValue.ToLongTimeString();
+             // returns 12:47:35 AM
+
+             resultLabel.Text = myValue.ToShortDateString();
+             // returns 12/22/17
+
+             resultLabel.Text = myValue.ToShortTimeString();
+             // returns 12:50 AM
+
+
+
+             // ===== Add and subtract days--note chaining of methods ===== 
+
+             resultLabel.Text = myValue.AddDays(2).ToString();
+             // returns 12/24/17 12:52:35 AM (OMG ... Santa's almost here!!!)
+
+             resultLabel.Text = myValue.AddMonths(-2).ToString();
+             // returns 10/22/17 12:54:00 AM
+
+
+             // ===== Isolates the month, day of week or year ===== 
+             resultLabel.Text = myValue.Month.ToString();
+             // returns 12
+
+             resultLabel.Text = myValue.DayOfWeek.ToString();
+             // returns Friday
+
+             resultLabel.Text = myValue.DayOfYear.ToString();
+             // retuns 356 
+
+             // ===== Boolean return for DST T/F ===== 
+             resultLabel.Text = myValue.IsDaylightSavingTime().ToString();
+
+
+
+             // ===== Using a specified date for a variable ===== 
+
+             DateTime myValue = DateTime.Parse("9/14/1969");
+             resultLabel.Text = myValue.ToLongDateString();
+             // returns September 14, 1969
+             // NOTE: Instructors version included day of week (Sunday, September 14, 1969)
+             resultLabel.Text = myValue.DayOfWeek.ToString();
+             // returns Monday
+
+
+            // ===== alternate way to specify date variable ===== 
+            DateTime myValue = new DateTime(1969, 9, 14, 10, 53, 0);
             resultLabel.Text = myValue.ToLongDateString();
+            // returns September 14, 1969 ... DOES NOT INCLUDE hours, minutes ...
 
         }
     }
---
+++ Challenge5_Dates_C#_DevU/CS-ASP_016_DaysBetweenDates_Final/CS-ASP_016_DaysBetweenDates_Final/Default.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_016_DaysBetweenDates_Final.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .auto-style2 {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: large;
+        }
+        .auto-style3 {
+            background-color: #CCFFCC;
+        }
+        .auto-style4 {
+            background-color: #CCFFFF;
+        }
+        .auto-style5 {
+            background-color: #CCCCFF;
+        }
+    </style>
+</head>
+<body>
+    <h2>
+        <br class="auto-style1" />
+        <span class="auto-style1">&nbsp; Challenge: Days Between Dates</span></h2>
+    <p class="auto-style1">
+        &nbsp;</p>
+    <form id="form1" runat="server">
+        <p class="auto-style2">
+            &nbsp;&nbsp;&nbsp;&nbsp; <span class="auto-style3"><strong>Calendar 1:&nbsp; Choose a Date</strong></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </p>
+        <p class="auto-style2">
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </p>
+        <p class="auto-style2">
+            &nbsp;</p>
+        <p class="auto-style2">
+            &nbsp; <span class="auto-style4"><strong>Calendar 2: Choose another Date</strong></span></p>
+        <p class="auto-style2">
+            <asp:Calendar ID="Calendar2" runat="server"></asp:Calendar>
+            &nbsp;</p>
+        <p class="auto-style2">
+            &nbsp;</p>
+        <p class="auto-style2">
+            &nbsp;</p>
+        <p class="auto-style2">
+            &nbsp;&nbsp;&nbsp;&nbsp; <span class="auto-style5"><strong>Click the OK button below for the magic ;-)</strong></span></p>
+        <p>
+            <span class="auto-style2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            <asp:Button ID="Button1" runat="server" CssClass="auto-style2" Text="Button" />
+            <span class="auto-style2">&nbsp;</span></p>
+        <p>
+            <span class="auto-style2">&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            <asp:Label ID="Label1" runat="server" CssClass="auto-style2" Text="Label"></asp:Label>
+            <span class="auto-style2">&nbsp;</span></p>
+        <p class="auto-style2">
+            &nbsp;</p>
+        <p class="auto-style2">
+            &nbsp;</p>
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
